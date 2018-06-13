4aa95969f63cec36cce3ecbe356f990c1ac39144
bean <>
2018-06-13 15:18:36 +0800

    提交项目第一次代码

---
+++ cocoaPod&FMDB/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  cocoaPod&FMDB
+//
+//  Created by bean on 2018/6/13.
+//  Copyright © 2018年 bean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ cocoaPod&FMDB/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  cocoaPod&FMDB
+//
+//  Created by bean on 2018/6/13.
+//  Copyright © 2018年 bean. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
